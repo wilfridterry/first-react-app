7ac696169ab2416358ff6bba0b75e14dec11b766
Stannis <zalevschi1998@gmail.com>
2022-07-05 01:27:25 +0300

    Added a handle for selecting on increase

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,31 +1,61 @@
+import { Component } from 'react';
 import './employees-list-item.css'
 
-const EmployeesListItem = ({user}) => {
-    let classList = 'list-group-item d-flex justify-content-between';
+class EmployeesListItem extends Component {
 
-    if (user.increase) {
-        classList += ' increase';
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            increase: props.user.increase,
+            isGoingIncrease: false
+        };
     }
 
-    return (
-        <li className={classList}>
-            <span className="list-group-item-label">{user.name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={user.salary + '$'}/>
-            <div className="d-flex justify-content-center">
-                <button 
-                    type="button"
-                    className="btn-cookie btm-sm">   
-                    <i className="fas fa-cookie"></i>
-                </button>
-                <button 
-                    type="button"
-                    className="btn-trash btm-sm">   
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
-};
+    handleIncreaseOnClick() {
+        this.setState(({increase}) => ({increase: !increase}))
+    }
+
+    handleSelectedForIncreseOnClick() {
+        this.setState(({isGoingIncrease}) => ({isGoingIncrease: !isGoingIncrease}));
+    }
+    render() {
+    
+        const {name, salary} = this.props.user;
+        
+        let classList = 'list-group-item d-flex justify-content-between';
+
+        if (this.state.increase) {
+            classList += ' increase';
+        }
+
+        if (this.state.isGoingIncrease) {
+            classList += ' like';
+        }
+
+        return (
+            <li className={classList}>
+                <span 
+                    className="list-group-item-label" 
+                    onClick={this.handleSelectedForIncreseOnClick.bind(this)}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className="d-flex justify-content-center">
+                    <button 
+                        type="button"
+                        className="btn-cookie btm-sm"
+                        onClick={this.handleIncreaseOnClick.bind(this)}>   
+                        <i className="fas fa-cookie"></i>
+                    </button>
+                    <button 
+                        type="button"
+                        className="btn-trash btm-sm">   
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
+}
 
 export default EmployeesListItem;
